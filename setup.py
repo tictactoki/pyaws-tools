dbbd8643d00e5dd690c7deb03e6dce6e1920f198
stephanewong <stephane.wong1@gmail.com>
2021-05-12 22:16:15 +0200

    Add seed

---
+++ setup.py
@@ -0,0 +1,42 @@
+"""tools module."""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+# Runtime Requirements.
+inst_reqs = ["click"]
+
+# Dev Requirements
+extra_reqs = {
+    "test": ["pytest", "pytest-cov"],
+    "dev": ["pytest", "pytest-cov", "pre-commit"],
+}
+
+
+setup(
+    name="pyaws-tools",
+    version="0.0.1",
+    description=u"python tools for aws",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires=">=3",
+    classifiers=[
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="An Awesome python module",
+    author=u"",
+    author_email="",
+    url="",
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=inst_reqs,
+    extras_require=extra_reqs,
+    entry_points={"console_scripts": ["tools = tools.scripts.cli:tools"]},
+)
